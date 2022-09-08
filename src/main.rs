534cb54a37f0ab0705018df084d0c61d868b222e
Hiranmaya Gundu <hiranmayagundu@gmail.com>
2022-09-07 20:34:49 -0700

    clean up the code

--- src/main.rs
+++ src/main.rs
@@ -13,41 +13,9 @@ const BOUND_IP_ADDR: &'static str = "10.1.3.3:0";
 const CAPACITY: u64 = 100 * 10_u64.pow(6);
 
 fn main() {
-    // println!("Hello, world!");
     if cfg!(target_os = "linux") != true {
         panic!("This program only works on Linux");
     }
-    // let mut addrs = mem::MaybeUninit::<*mut libc::ifaddrs>::uninit();
-    // let result = unsafe { getifaddrs(addrs.as_mut_ptr()) };
-    // println!("Result: {}", result);
-    // println!("{:?}", addrs);
-    // let mut ifaddrs = unsafe { addrs.assume_init() };
-    // while ifaddrs != std::ptr::null_mut() {
-    //     let ifa_name = unsafe { ffi::CStr::from_ptr((*ifaddrs).ifa_name) }
-    //         .to_string_lossy()
-    //         .to_string();
-    //     let ifa_addr = unsafe { (*ifaddrs).ifa_addr };
-    //     let result = unsafe {
-    //         let family = (*ifa_addr).sa_family;
-    //         let ipv4 = libc::AF_INET.abs() as u16;
-    //         let ipv6 = libc::AF_INET6.abs() as u16;
-    //         match family {
-    //             ipv4 => {
-    //                 let addr = (*ifa_addr).sa_data;
-    //                 let addr = SocketAddr::from((addr, 0));
-    //                 println!("{}: {}", ifa_name, addr);
-    //             }
-    //             ipv6 => {
-    //                 let addr = (*ifa_addr).sa_data;
-    //                 let addr = SocketAddr::from((addr, 0));
-    //                 println!("{}: {}", ifa_name, addr);
-    //             }
-    //             _ => {}
-    //         }
-    //     };
-    //     ifaddrs = unsafe { (*ifaddrs).ifa_next };
-    //     println!("Interface: {}", ifa_name);
-    // }
 
     let mut file = OpenOptions::new()
         .read(true)
@@ -65,20 +33,11 @@ fn main() {
     for interface in interfaces {
         match interface.address {
             Some(address) => {
-                // println!("interface {} address {}", interface.interface_name, address);
                 if address.to_string() == BOUND_IP_ADDR {
                     interface_name = interface.interface_name;
                 }
-                // if address.to_string() == "127.0.0.1:0" {
-                //     interface_name = interface.interface_name;
-                // }
             }
-            None => {
-                // println!(
-                //     "interface {} with unsupported address family",
-                //     interface.interface_name
-                // );
-            }
+            None => {}
         }
     }
 
@@ -98,8 +57,6 @@ fn main() {
         let devices = dev_parser::get();
         for device in devices {
             if device.interface == interface_name {
-                // println!("{}: {}", device.interface, device.receive_bytes);
-
                 let transmit_bytes = device.transmit_bytes - old_stats.transmit_bytes;
                 let receive_bytes = device.receive_bytes - old_stats.receive_bytes;
                 file.write_all(
@@ -118,9 +75,9 @@ fn main() {
                 )
                 .expect("Failed to write data");
                 let transmit_capacity =
-                    ((receive_bytes as f64 * 8_f64) / (CAPACITY as f64)) * 100_f64;
-                let receive_capacity =
                     ((transmit_bytes as f64 * 8_f64) / (CAPACITY as f64)) * 100_f64;
+                let receive_capacity =
+                    ((receive_bytes as f64 * 8_f64) / (CAPACITY as f64)) * 100_f64;
 
                 if transmit_capacity >= 90.0 || receive_capacity >= 90.0 {
                     println!(
@@ -152,9 +109,4 @@ fn main() {
         }
         thread::sleep(Duration::from_secs(1));
     }
-
-    // let contents =
-    //     fs::read_to_string(PROC_NET_DEV).expect("Failed to read the file at /proc/net/dev");
-
-    // println!("With text:\n{contents}");
 }
